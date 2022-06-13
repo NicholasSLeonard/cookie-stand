71873f8b196464340170f180ef9c8486133ff4ca
Nicholas Leonard <nicholas_leonard@live.com>
2022-06-13 19:03:33 -0400

    created objects to store information for locations

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let seattle =
+{
+    minCustomers:23,
+    maxCustomers:65,
+    avgSales:6.3,
+    hourlySales:[]
+};
+let tokyo =
+{
+    minCustomers:3,
+    maxCustomers:24,
+    avgSales:1.2,
+    hourlySales:[]
+};
+let dubai =
+{
+    minCustomers:11,
+    maxCustomers:38,
+    avgSales:3.7,
+    hourlySales:[]
+};
+let paris =
+{
+    minCustomers:20,
+    maxCustomers:38,
+    avgSales:2.3,
+    hourlySales:[]
+};
+let lima =
+{
+    minCustomers:2,
+    maxCustomers:16,
+    avgSales:4.6,
+    hourlySales:[]
+};
