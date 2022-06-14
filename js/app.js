ed92d69b4d3b118ab5b05868339beebb67fae58a
Nicholas Leonard <nicholas_leonard@live.com>
2022-06-14 19:51:07 -0400

    created new city constructor

--- js/app.js
+++ js/app.js
@@ -1,50 +1,50 @@
 let dayHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-let seattle =
-{
-  minCustomers: 23,
-  maxCustomers: 65,
-  avgSales: 6.3,
-  hourlySales: [],
-  total: 0,
-  cityName: 'Seattle'
-};
-let tokyo =
-{
-  minCustomers: 3,
-  maxCustomers: 24,
-  avgSales: 1.2,
-  hourlySales: [],
-  total: 0,
-  cityName: 'Tokyo'
-};
-let dubai =
-{
-  minCustomers: 11,
-  maxCustomers: 38,
-  avgSales: 3.7,
-  hourlySales: [],
-  total: 0,
-  cityName: 'Dubai'
-};
-let paris =
-{
-  minCustomers: 20,
-  maxCustomers: 38,
-  avgSales: 2.3,
-  hourlySales: [],
-  total: 0,
-  cityName: 'Paris'
-};
-let lima =
-{
-  minCustomers: 2,
-  maxCustomers: 16,
-  avgSales: 4.6,
-  hourlySales: [],
-  total: 0,
-  cityName: 'Lima'
-};
+// let seattle =
+// {
+//   minCustomers: 23,
+//   maxCustomers: 65,
+//   avgSales: 6.3,
+//   hourlySales: [],
+//   total: 0,
+//   cityName: 'Seattle'
+// };
+// let tokyo =
+// {
+//   minCustomers: 3,
+//   maxCustomers: 24,
+//   avgSales: 1.2,
+//   hourlySales: [],
+//   total: 0,
+//   cityName: 'Tokyo'
+// };
+// let dubai =
+// {
+//   minCustomers: 11,
+//   maxCustomers: 38,
+//   avgSales: 3.7,
+//   hourlySales: [],
+//   total: 0,
+//   cityName: 'Dubai'
+// };
+// let paris =
+// {
+//   minCustomers: 20,
+//   maxCustomers: 38,
+//   avgSales: 2.3,
+//   hourlySales: [],
+//   total: 0,
+//   cityName: 'Paris'
+// };
+// let lima =
+// {
+//   minCustomers: 2,
+//   maxCustomers: 16,
+//   avgSales: 4.6,
+//   hourlySales: [],
+//   total: 0,
+//   cityName: 'Lima'
+// };
 
 function genRandom(min, max) {
   return (Math.round(Math.random() * (max - min) + min));
@@ -84,6 +84,21 @@ function newDay() {
     calcTotal(lima);
   }
 }
+
+function newCity(minCustomers, maxCustomers, avgSales, cityName)
+{
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.avgSales = avgSales;
+  let hourlySales = [];
+  let total = 0;
+  this.cityName = cityName;
+}
+let seattle = new newCity(23, 65, 6.3, 'seattle');
+let tokyo = new newCity(23, 65, 6.3, 'seattle');
+let dubai = new newCity(23, 65, 6.3, 'seattle');
+let paris = new newCity(23, 65, 6.3, 'seattle');
+let lima = new newCity(23, 65, 6.3, 'seattle');
 
 newDay();
 displayList(seattle);
